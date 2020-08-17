c2553992ad20d2b17e84c03d4b1914b74aa3e208
JLC <jlc@annonymous.com>
2020-08-17 09:25:49 -0500

    Organizing code

--- script.js
+++ script.js
@@ -7,43 +7,8 @@ let userLat, userLon;​
 function initMap() {
     "use strict";
     var map = new Microsoft.Maps.Map('#map');
-
-
 }
-/*     //creating the new infoWindow object in map and assigning to our variable
-    infoWindow = new google.maps.InfoWindow;​
-    
-    //Getting user's Geolocation from their web browser
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-                lat: position.coords.latitude,
-                lng: position.coords.longitude
-            };
-            userLat = pos.lat;
-            userLon = pos.lng;
-            //setting the position obtained with geolocation
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('You are here Honeydew');
-            //adding the infowindow object to our map
-            infoWindow.open(map);
-            // Centering the map to our position
-            map.setCenter(pos);
-        }, function() { //handling browser errors
-            handleLocationError(true, infoWindow, map.getCenter());
-        });
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoWindow, map.getCenter());
-    }
-}​
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-    infoWindow.setPosition(pos);
-    infoWindow.setContent(browserHasGeolocation ?
-        'Error: The Geolocation service failed.' :
-        'Error: Your browser doesn\'t support geolocation.');
-    infoWindow.open(map);
-} */
+
 
 // hiking
 $(".hikingBtn").on("click", function() {
